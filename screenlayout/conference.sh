be370a28eaeb3291516a392b6afae409aa661a3b
Mark Kettner <mark.kettner@offis.de>
2021-05-12 16:26:32 +0200

    fix xrandr command for nbim04

--- screenlayout/conference.sh
+++ screenlayout/conference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case $(hostname) in
-    nbim04 )
-        xrandr --output VGA-1 --off --output DP-2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+    nbim05 )
+        xrandr --output HDMI-1-2 --off --output HDMI-1-1 --off --output DP-1-2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VGA-1-1 --off --output DP-1-2-2 --off --output DP-1-2-3 --off --output DP-1-2 --off --output DP-1-1 --off
         ;;
     t460p )
         xrandr --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off --output DP-1-3 --off --output HDMI-1-3 --off --output DP-1-3-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-3-2 --off --output DP-1-3-3 --off
