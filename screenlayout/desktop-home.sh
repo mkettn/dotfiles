d5f76519322d6f635c1425f1be88937daadb98f8
Mark Kettner <mark.kettner@offis.de>
2021-06-08 15:07:00 +0200

    change hostname

--- screenlayout/desktop-home.sh
+++ screenlayout/desktop-home.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 case $(hostname) in
-    nbim04)
+    nbim05 )
         xrandr --output HDMI-1-2 --off --output HDMI-1-1 --off --output DP-1-2-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --off --output VGA-1-1 --off --output DP-1-2-2 --off --output DP-1-2-3 --off --output DP-1-2 --off --output DP-1-1 --off
         ;;
-    t460p)
+    t460p )
         xrandr --output eDP-1-1 --off --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off --output DP-1-3 --off --output HDMI-1-3 --off --output DP-1-3-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-3-2 --off --output DP-1-3-3 --off
         ;;
 esac
